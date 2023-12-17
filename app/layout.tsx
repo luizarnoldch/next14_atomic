b1b0c6fb23cd2e3e3b8968d190ae861773648538
luizarnoldch <luizarnoldch@gmail.com>
2023-12-17 00:30:13 -0500

    vercel analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
                 <div className="w-full bg-transparent">
                     {children}
                 </div>
+                <Analytics />
             </body>
         </html>
     );
