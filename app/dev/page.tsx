0a65269c296ebbf1da9bfdc3799be9ed4115e462
luizarnoldch <luizarnoldch@gmail.com>
2023-12-24 15:59:59 -0500

    scroll change color

---
+++ app/dev/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+const VerticalNav = () => {
+    
+    return (
+        <>
+        <div className="w-full h-full bg-black">
+            Page
+        </div>
+        <div className="w-full h-full bg-black">
+            Page
+        </div>
+        </>
+    );
+};
+
+export default VerticalNav;
--- components/line/ScroolLine.tsx
+++ components/line/ScroolLine.tsx
@@ -1,31 +1,51 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 const VerticalNav = () => {
-	const [markerPosition, setMarkerPosition] = useState(0)
+    const [markerPosition, setMarkerPosition] = useState(0);
+    const [markerColor, setMarkerColor] = useState('bg-white');
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const newPosition =
-				window.scrollY /
-				(document.body.scrollHeight - window.innerHeight)
-			setMarkerPosition(newPosition * 100)
-		}
+    useEffect(() => {
+        const handleScroll = () => {
+            const screenHeight = window.innerHeight - 250;
+            const newPosition = window.scrollY / (document.body.scrollHeight - screenHeight);
+            setMarkerPosition(newPosition * 100);
 
-		window.addEventListener('scroll', handleScroll)
-		return () => window.removeEventListener('scroll', handleScroll)
-	}, [])
+            // Cambiar el color del marcador cada 100vh
+            const scrollSection = Math.floor(window.scrollY / screenHeight);
+            let color;
+            switch (scrollSection % 3) {
+                case 0:
+                    color = 'bg-white';
+                    break;
+                case 1:
+                    color = 'bg-black';
+                    break;
+                case 2:
+                    color = 'bg-green-500';
+                    break;
+                default:
+                    color = 'bg-white';
+            }
+            setMarkerColor(color);
+        };
 
-	return (
-		<div className='fixed z-10 ml-24 h-screen'>
-			<div className='absolute left-0 h-full w-[1px] rounded-md bg-white opacity-30'></div>
-			<div
-				className='absolute left-0 h-[5rem] w-[1px] bg-white'
-				style={{ top: `${markerPosition}%` }}
-			></div>
-		</div>
-	)
-}
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
-export default VerticalNav
+    return (
+        <div className='fixed z-10 ml-24 h-screen'>
+            <div className='absolute left-0 h-full w-[1px] rounded-md bg-white opacity-30'></div>
+            <div
+                className={`absolute left-0 h-[5rem]  w-[1px] ${markerColor}`}
+                style={{ top: `${markerPosition}%` }}
+            ></div>
+        </div>
+    );
+};
+
+export default VerticalNav;
+
+
